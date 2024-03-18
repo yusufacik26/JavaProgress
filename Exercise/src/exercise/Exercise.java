0f1d9afd4b66a9a150b8061613987013bf7444e8
yusufacik26 <yusufacikedu@gmail.com>
2024-03-18 20:08:46 +0300

    Exercise RecursiveFunctions

---
+++ Exercise/src/exercise/Exercise.java
@@ -0,0 +1,48 @@
+
+package exercise;
+
+
+public class Exercise {
+  /*   public static int word(String s , char c ,int i ){
+   if( i == s.length()) return 0;
+   
+   else if ( s.charAt(i)==c){
+    return 1+word(s,c,i+1);
+   }
+   else return 0+ word(s,c,i+1);
+  
+  }*/
+  /*  public static int rakam(int a){
+    if(a%10==0) return 0;
+    else return (a%10)+rakam(a/10);
+    }*/
+   /*public static int buyuk(String s , int i ){   
+    if(i == s.length()) return 0 ;
+    else if ( s.charAt(i)>='A'&& s.charAt(i)<='Z') return 1+buyuk(s,i+1);   
+    else return 0+buyuk(s,i+1);
+   
+   }*/
+     /*  public static int chardizi(char[]c,char a,int i )  {
+       if(i==c.length) return 0;
+       else if (c[i]==a)return 1 + chardizi(c,a,i+1);
+       else return 0 + chardizi(c,a,i+1);
+       
+       
+       
+       }  */
+   
+   
+   
+    
+    public static void main(String[] args) {
+      // System.out.println(word("yusuf",'u',0));
+      //  System.out.println(rakam(12343));
+       // System.out.println(buyuk("YuSuFaCiK",0));
+      /*char c []={'a','b','a','c','a'};
+      
+        System.out.println(chardizi(c,'a',0));*/
+      
+      
+    }
+    
+}
