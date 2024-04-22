b58b0c02b6b3d7ba33a5301f305e8c7f0c85c60b
yusufacik26 <yusufacikedu@gmail.com>
2024-04-22 22:15:40 +0300

    ClassCalisma1

---
+++ ClassDegerReferans/src/Deger.java
@@ -0,0 +1,24 @@
+
+
+
+public class Deger {
+    
+    public static void main(String[] args) {
+        int s1=10;
+    int s2=20;
+    s2 = s1;
+    s1 = 30;
+        System.out.println(s2);
+        
+    int sayilar1[] = new int[]{1,2,3};
+    int sayilar2[]= new int[]{3,4,5};
+    
+    sayilar2=sayilar1;
+    sayilar1[0]=31;
+        System.out.println(sayilar2[0]);
+        
+        
+        
+    }
+    
+}
---
+++ ClassDegerReferans/src/classdegerreferans/ClassDegerReferans.java
@@ -0,0 +1,11 @@
+
+package classdegerreferans;
+
+public class ClassDegerReferans {
+
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
