ec4e9654a89fe7590db5ab66693b19f6fbaca3aa
Yusuf Acik <yusufacikedu@gmail.com>
2024-05-24 15:40:09 +0300

    thread çalışma

---
+++ Threadİsim yazdırma/src/threadisim/yazdırma/ThreadİsimYazdırma.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package threadisim.yazdırma;
+
+/**
+ *
+ * @author Yusuf
+ */
+public class ThreadİsimYazdırma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
