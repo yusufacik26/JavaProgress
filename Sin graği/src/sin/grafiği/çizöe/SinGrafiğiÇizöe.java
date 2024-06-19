7e928a6f6338d0df2aee0a492810d1223fb417c0
Yusuf Acik <yusufacikedu@gmail.com>
2024-06-19 22:47:35 +0300

    2Sin(x)+10 grafiği çizme

---
+++ Sin graği/src/sin/grafiği/çizöe/SinGrafiğiÇizöe.java
@@ -0,0 +1,55 @@
+
+package sin.grafiği.çizöe;
+
+import javax.swing.*;
+import java.awt.*;
+
+class ciz extends JPanel{
+  
+    
+    
+     
+    protected void paintComponent(Graphics g){
+     super.paintComponent(g);
+     
+      int w= getWidth();
+    int h = getHeight();
+    int amp= 100;
+    int fre=15;
+    
+    int xn[]= new int[w];
+    int yn[]= new int[w];
+    
+        for (int i = 0; i < w; i++) { 
+            double y=  h/2+ amp * 2*Math.sin((2* Math.PI * fre*i)/w)+10; // 2 * Sin(x) + 10 grafiği
+            xn[i]=i;
+            yn[i]=(int)y;
+            
+        }
+     
+        g.setColor(Color.BLACK);
+        g.drawPolyline(xn, yn, w);
+    
+    }
+
+}
+
+public class SinGrafiğiÇizöe extends JFrame{
+    
+public SinGrafiğiÇizöe(){
+ 
+    add(new ciz());
+
+}
+    
+    public static void main(String[] args) {
+        SinGrafiğiÇizöe p = new SinGrafiğiÇizöe();
+        p.setTitle("sin grafik");
+        p.setSize(800,400);
+        p.setResizable(true);
+        p.setLocation(400, 400);
+        p.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        p.setVisible(true);
+    }
+    
+}
